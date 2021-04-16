658be78edddef99f314c346b336206c6225d3ce7
Phuong Dong <phamphuongdong2001@gmail.com>
2021-04-16 15:54:30 +0700

    16/4/2021

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -1,1 +1,19 @@
-document.getElementById("demo").innerHTML = 11;
+var readMore = document.querySelector('#showmore');
+var userBlock = document.querySelectorAll('#propose .user-block');
+for (var index = 0; index < userBlock.length && index < 4; ++index) {
+    userBlock[index].style.display = 'block';
+}
+
+readMore.addEventListener('click', (e) => {
+    for (var index = 4; index < userBlock.length; ++index) {
+        if (userBlock[index].style.display == 'block') {
+            userBlock[index].style.display = 'none';
+            readMore.innerHTML = "Xem thêm";
+
+        } else {
+            userBlock[index].style.display = 'block';
+            readMore.innerHTML = "Ẩn bớt";
+
+        }
+    }
+});
